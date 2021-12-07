0b764fec52e78275c33f0aab5cd343980529be77
nguyenthanhloc-nlu <19130125@st.hcmuaf.edu.vn>
2021-12-07 21:29:24 +0700

    role

--- role.sql
+++ role.sql
@@ -1,58 +1,46 @@
 ﻿
--- tạo/xóa user -- 
+-- tạo login --
 CREATE LOGIN thanhloc
 WITH PASSWORD = '123456'
+
+-- tạo user -- 
 CREATE USER user1 FOR LOGIN thanhloc
+
+-- xóa user -- 
 DROP user user1
 
 
--- ◦ Statement permission -- 
-/*
-◦ CREATE DATABASE
-◦ CREATE DEFAULT
-◦ CREATE PROCEDURE
-◦ CREATE RULE
-◦ CREATE TABLE
-◦ CREATE VIEW
-◦ BACKUP DATABASE
-◦ BACKUP LOG
-*/
+-- gán quyền "Statement permission" cho user-- 
 GRANT CREATE DATABASE, CREATE TABLE TO user1
 
-
--- ◦ Object permission -- 
-/*
-DELETE table , view 
-EXECUTE stored procedure 
-INSERT table , view 
-SELECT table, view, và column 
-UPDATE table, view, và column
-*/
-GRANT SELECT, UPDATE
-ON Employee (emp_fname, emp_lname)
+-- gán quyền "Object permission" -- 
+GRANT SELECT, UPDATE, INSERT, DELETE, EXECUTE
+ON SAN_PHAM
 TO user1
 
 
---Tạo/xóa/thay đổi role -- 
--- tạo role cho phép đọc, sửa 1 bảng
-go
-CREATE ROLE quanly AUTHORIZATION user1
-DROP role quanly
--- gán quyền vào role
-GRANT SELECT, INSERT, UPDATE
+-- tạo role cho phép đọc, sửa 1 bảng sản phẩm -- 
+CREATE ROLE SAN_PHAM_ROLE AUTHORIZATION user1
+
+-- xóa role cho phép đọc, sửa 1 bảng sản phẩm -- 
+DROP role SAN_PHAM_ROLE
+
+
+-- gán quyền vào role SAN_PHAM_ROLE
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE
 ON DBO.SAN_PHAM
-TO QUANLY
+TO SAN_PHAM_ROLE
 GO
 
- -- xóa quyền trên role
+ -- xóa quyền SELECT, INSERT, UPDATE trên role
 REVOKE  SELECT, INSERT, UPDATE
 ON DBO.SAN_PHAM
-TO QUANLY
+TO SAN_PHAM_ROLE
 GO
 
--- them user vào role -- 
+-- them user vào role
 sp_addRoleMember 'quanly', 'user1';
 
--- xóa user khỏi role -- 
+-- thu hồi của user tren role -- 
 go
 sp_droprolemember 'quanly', 'user1'
